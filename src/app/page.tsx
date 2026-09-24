0a0e0e00242c62effbb6e5d23428eae9f6ba1a19
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-09-24 14:40:32 +0200

    Move navigation into shared app shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
 import { AttentionSection } from "../components/AttentionSection";
-import { MainNavigation } from "../components/MainNavigation";
 import { UpcomingDatesSection } from "../components/UpcomingDatesSection";
 import { UserContextHeader } from "../components/UserContextHeader";
 import { demoToday } from "../data/demo";
 
 export default function Home() {
-  return <main className="app-shell">
-    <MainNavigation
-      organizationName={demoToday.organization.name}
-      organizationRole={demoToday.organization.role}
+  return <>
+    <UserContextHeader
+      dateLabel={demoToday.dateLabel}
+      firstName={demoToday.firstName}
+      subtitle={demoToday.subtitle}
     />
-    <section className="workspace">
-      <UserContextHeader
-        dateLabel={demoToday.dateLabel}
-        firstName={demoToday.firstName}
-        subtitle={demoToday.subtitle}
-      />
-      <div className="content">
-        <AttentionSection />
-        <UpcomingDatesSection />
-      </div>
-    </section>
-  </main>;
+    <div className="content">
+      <AttentionSection />
+      <UpcomingDatesSection />
+    </div>
+  </>;
 }
