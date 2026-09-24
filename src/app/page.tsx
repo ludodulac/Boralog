fbd1a77c25cdc0c3da899c77fd48fdd8fe8ef47c
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-09-24 16:06:34 +0200

    Connect shell to authenticated identity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@ import { RecentActivitySection } from "../components/RecentActivitySection";
 import { UpcomingDatesSection } from "../components/UpcomingDatesSection";
 import { UserContextHeader } from "../components/UserContextHeader";
 import { demoToday } from "../data/demo";
+import { getCurrentIdentity } from "../lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const identity = await getCurrentIdentity();
+  const firstName = identity.profile?.display_name?.split(/\s+/)[0] || "vous";
   return <>
-    <UserContextHeader
-      dateLabel={demoToday.dateLabel}
-      firstName={demoToday.firstName}
-      subtitle={demoToday.subtitle}
-    />
+    <UserContextHeader dateLabel={demoToday.dateLabel} firstName={firstName} subtitle={demoToday.subtitle}/>
     <div className="content">
       <AttentionSection />
       <RecentActivitySection />
