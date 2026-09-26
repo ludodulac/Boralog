762c7187e011be4248eb77bf878e33c2c2cb4915
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-09-26 18:27:54 +0200

    test: move diagnostic 056 under auth bypass

--- src/app/diagnostic-056/page.tsx
+++
@@ -1,8 +0,0 @@
-import { CreateOrganizationForm } from "../organisations/nouvelle/CreateOrganizationForm";
-
-export default function Diagnostic056Page() {
-  return <main>
-    <h1>DIAGNOSTIC_056</h1>
-    <CreateOrganizationForm attemptId="00000000-0000-4000-8000-000000000056" />
-  </main>;
-}
