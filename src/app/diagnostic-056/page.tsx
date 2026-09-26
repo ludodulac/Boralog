7cc72a274b117c7b7aac49835871ddb9b5fc7ee7
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-09-26 18:23:59 +0200

    test: add isolated server action diagnostic page 056

---
+++ src/app/diagnostic-056/page.tsx
@@ -0,0 +1,8 @@
+import { CreateOrganizationForm } from "../organisations/nouvelle/CreateOrganizationForm";
+
+export default function Diagnostic056Page() {
+  return <main>
+    <h1>DIAGNOSTIC_056</h1>
+    <CreateOrganizationForm attemptId="00000000-0000-4000-8000-000000000056" />
+  </main>;
+}
